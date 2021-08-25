7793feacda4c2c8a3acaddb80da2cdb91107c542
nuhcolakkadioglu <hex-tr@hotmail.com>
2021-08-25 11:28:08 +0300

    modüllerin oluşturulması

---
+++ app.js
@@ -0,0 +1,32 @@
+//Storage Controller
+const StorageController = (function(){
+
+})();
+
+//Product Controller
+const ProductController=(function(){
+
+    //private alanlar
+
+
+    //public alan
+    return {
+        
+    }
+        
+
+})();
+
+//UI Controller
+const UIController = (function(){
+
+})();
+
+
+//App Controller
+
+const AppController = (function(ProductCtrl,UICtrl){
+
+
+
+})(ProductController,UIController);
